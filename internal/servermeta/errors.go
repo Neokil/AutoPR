3fcbcae4d90230dfcd41df92d97e69b166dc56a4
Simon Schulte <github@neokil.de>
2026-04-27 09:17:52 +0200

    restructure server and domain packages

--- internal/servermeta/errors.go
+++
@@ -1,7 +0,0 @@
-// Package servermeta manages the server-side metadata store: repos, tickets, and background jobs.
-package servermeta
-
-import "errors"
-
-// ErrJobNotFound is returned when a job ID does not exist in the store.
-var ErrJobNotFound = errors.New("job not found")
